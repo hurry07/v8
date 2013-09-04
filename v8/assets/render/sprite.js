96780404289c6cd16128b72a6988b3bc216417c7
jie <hurry07@163.com>
2013-09-04 18:52:15 +0800

    simplify sprite, so it can be extend in future

--- v8/assets/render/sprite.js
+++ v8/assets/render/sprite.js
@@ -1,7 +1,7 @@
-var createMesh = require('glcore/meshbuffer.js').createMesh;
 var _gl = require('opengl');
 var _geometry = require('core/glm.js');
 var _MeshNode = require('render/meshnode.js');
+var _createMesh = require('glcore/meshbuffer.js').createMesh;
 var _inherit = require('core/inherit.js');
 
 var _glm = _geometry.glm;
@@ -14,34 +14,37 @@ var _order = [0, 0, 0, 1, 1, 0, 1, 1];
  * @param frame texture frame
  * @constructor
  */
-function Sprite(material, frame) {
-    _MeshNode.call(this, createMesh('p3t2', 4, _gl.TRIANGLE_STRIP), material);
+function SpriteNode(material, frame) {
+    _MeshNode.call(this, this.createMesh(), material);
 
     this.mFrame = frame;
-    this.mAccessorP = this.mBuffer.accessor('p');
-    this.mAccessorT = this.mBuffer.accessor('t');
     this.setSize(frame.width(), frame.height());
-
     this.initMesh();
 }
-_inherit(Sprite, _MeshNode);
-Sprite.prototype.initMesh = function () {
-    var f = this.mFrame;
-    var m = f.getMatrix();
+_inherit(SpriteNode, _MeshNode);
+SpriteNode.prototype.createMesh = function () {
+    return _createMesh('p3t2', 4, _gl.TRIANGLE_STRIP);
+}
+SpriteNode.prototype.initMesh = function () {
     var t = new _v2();
     var v = new _v3();
     var b = this.mBuffer;
+
+    var accp = b.accessor('p');
+    var acct = b.accessor('t');
+    var f = this.mFrame;
+    var m = f.getMatrix();
 
     for (var i = 0; i < 8; i += 2) {
         f.getPoint(v, t, _order[i], _order[i + 1]);
         _glm.mulMV3(v, m, v);
 
-        this.mAccessorT.set(t);
-        this.mAccessorP.set(v);
+        acct.set(t);
+        accp.set(v);
         b.push(i / 2);
     }
 
     b.upload();
 }
 
-module.exports = Sprite;
+module.exports = SpriteNode;
