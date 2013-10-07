740e9086b63df8951306900d89bff178b7905b4a
JackLee <freefoxser@gmail.com>
2013-10-08 02:15:46 +0800

    add QueryContext

--- v8/assets/component/selector.js
+++ v8/assets/component/selector.js
@@ -7,6 +7,32 @@ var _SelectorListener = _listener.SelecterListener;
 var _selector = require('component/selector/selector.js');
 var _SelectorGroup = _selector.SelectorGroup;
 
+/**
+ * for reusable purpase
+ *
+ * @param node
+ * @constructor
+ */
+function QueryContext(node) {
+    this.root = _CSSNode.wrap(node);
+    this.itor = new _NodeIterator();
+    this.listener = new _SelectorListener().reset();
+}
+QueryContext.prototype.querySelector = function (pattern) {
+    var selectors = new _Parser().parse(pattern);
+    if (selectors.length == 0) {
+        return [];
+    }
+    return this.itor.childFirst(this.root, this.listener.reset(new _SelectorGroup(selectors)));
+}
+QueryContext.prototype.querySelectorGroup = function (group) {
+    return this.itor.childFirst(this.root, this.listener.reset(group));
+}
+
+function queryContext(node, query) {
+    return new QueryContext(node, query);
+}
+
 function querySelector(node, query) {
     var selectors = new _Parser().parse(query);
     if (selectors.length == 0) {
@@ -22,3 +48,4 @@ function querySelector(node, query) {
 }
 
 exports.querySelector = querySelector;
+exports.queryContext = queryContext;
