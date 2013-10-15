9a587d8bee8ac5cd67bb08e746454ee38ff85d88
jie <hurry07@163.com>
2013-10-15 13:14:35 +0800

    close statment

--- v8/assets/scenes/game/gamedata.js
+++ v8/assets/scenes/game/gamedata.js
@@ -1,22 +1,22 @@
 var Data = {
-}
+};
 var BeltData = {
     basicbet: 20,
     maxbet: 10,
     multip: 5
-}
+};
 
 function GameData() {
     this.level = 0;
 }
 GameData.prototype.getData = function (name) {
     return Data[name];
-}
+};
 GameData.prototype.getGameConf = function () {
     return {
         row: 7, col: 6, minmatch: 3, unitwidth: 50, headers: 1
     }
-}
+};
 
 // ==========================
 // ==========================
@@ -30,55 +30,55 @@ function Belt() {
 }
 Belt.prototype.getBet = function () {
     return this.bet * BeltData.basicbet;
-}
+};
 Belt.prototype.updateCost = function () {
     var mul = this.bet * BeltData.basicbet;
     this.cost = mul * this.multip;
     return mul;
-}
+};
 Belt.prototype.increaseBet = function () {
     this.bet++;
     if (this.bet > BeltData.maxbet) {
         this.bet = BeltData.maxbet;
     }
     return this.updateCost();
-}
+};
 Belt.prototype.decreaseBet = function () {
     this.bet--;
     if (this.bet <= 0) {
         this.bet = 1;
     }
     return this.updateCost();
-}
+};
 Belt.prototype.getMultipCount = function () {
     return BeltData.multip;
-}
+};
 Belt.prototype.setMultip = function (i) {
     this.multip = i + 1;
     this.updateCost();
-}
+};
 Belt.prototype.getCost = function () {
     return this.cost;
-}
+};
 Belt.prototype.spend = function (cost) {
     this.total -= cost;
-}
+};
 /**
  * @returns {number}
  */
 Belt.prototype.getTotal = function () {
     return this.total;
-}
+};
 Belt.prototype.getCurrent = function () {
     return this.current;
-}
+};
 Belt.prototype.getCurrent = function () {
     return this.current;
-}
+};
 
 Belt.prototype.onMatch = function (count, type) {
     return this.total += count * (type + 1) * this.bet;
-}
+};
 
 exports.gamedata = new GameData();
 exports.beltdata = new Belt();
