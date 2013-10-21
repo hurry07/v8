82cf07da0e3c475f2cf1dba0a4a67448858bf8bb
jie <hurry07@163.com>
2013-10-21 14:49:05 +0800

    orginaze import

--- release.android/src/com/op/activity/JSSurfaceView.java
+++ release.android/src/com/op/activity/JSSurfaceView.java
@@ -36,10 +36,8 @@ import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
 import javax.microedition.khronos.egl.EGLDisplay;
-import javax.microedition.khronos.opengles.GL10;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
